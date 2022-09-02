19eebed569ee5b9f8bd403634d28fd4ab9a26762
TrevorCash <cashtrevor@msn.com>
2022-09-01 18:17:12 -0600

    cmake cleanup

--- opencltest/jgjg.tsx
+++
@@ -1,9 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.0" name="jgjg" tilewidth="16" tileheight="16" tilecount="256" columns="16">
- <image source="TileSet.png" width="256" height="256"/>
- <wangsets>
-  <wangset name="Unnamed Set" type="mixed" tile="-1">
-   <wangcolor name="" color="#ff0000" tile="-1" probability="1"/>
-  </wangset>
- </wangsets>
-</tileset>
